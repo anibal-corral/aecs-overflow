56c0d90902bc1dc08ddbd6f457c55f4883b1c873
Anibal Corral Serrano <anibal.corral@gmail.com>
2020-09-17 10:37:02 -0300

    Genera el Header de la aplicacion

--- aecs-overflow/src/app/material.modules.ts
+++ aecs-overflow/src/app/material.modules.ts
@@ -5,15 +5,18 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 const modules = [
   MatButtonModule,
   MatCheckboxModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatIconModule
 ]
 @NgModule({
 
-  imports: modules
+  imports: modules,
+  exports: modules
 
 })
 
