b2ab139fd5d312dde339974152e7909802ea10fb
Anibal Corral Serrano <anibal.corral@gmail.com>
2020-09-17 09:58:50 -0300

    Agrega el modulo para usar el checkbox.

--- aecs-overflow/src/app/material.modules.ts
+++ aecs-overflow/src/app/material.modules.ts
@@ -3,10 +3,11 @@ Este archivo define los modulos de angular material que se van a utilizar.
 */
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 const modules = [
-  MatButtonModule
+  MatButtonModule,
+  MatCheckboxModule
 ]
 @NgModule({
 
