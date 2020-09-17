612af3f9596f08a80d5824b1fe5fefce4eada143
Anibal Corral Serrano <anibal.corral@gmail.com>
2020-09-17 10:16:39 -0300

    Agrega Toolbar a los modulos

--- aecs-overflow/src/app/material.modules.ts
+++ aecs-overflow/src/app/material.modules.ts
@@ -4,10 +4,12 @@ Este archivo define los modulos de angular material que se van a utilizar.
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const modules = [
   MatButtonModule,
-  MatCheckboxModule
+  MatCheckboxModule,
+  MatToolbarModule
 ]
 @NgModule({
 
