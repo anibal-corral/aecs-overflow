bd162f2df1d33cd00287d551c53fee0289363424
Anibal Corral Serrano <anibal.corral@gmail.com>
2020-09-20 16:22:53 -0300

    Agrego los campos al formulario

--- aecs-overflow/src/app/question/question-form.component.ts
+++ aecs-overflow/src/app/question/question-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
+import {Question} from './question-model';
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html',
@@ -11,5 +12,11 @@ export class QuestionFormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+onSubmit(form:NgForm){
+  const q = new Question (
+    form.value.title,
+    form.value.description
+  );
+  console.log(q);
+}
 }
