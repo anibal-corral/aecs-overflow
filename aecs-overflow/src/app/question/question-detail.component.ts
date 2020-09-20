a66f434a5a8d7125234eabddcb309a3057d78ff3
Anibal Corral Serrano <anibal.corral@gmail.com>
2020-09-20 11:39:57 -0300

    Utiliza mat-card para dar formato Crea archivo css para el detalle de la pregunta

--- aecs-overflow/src/app/question/question-detail.component.ts
+++ aecs-overflow/src/app/question/question-detail.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 import { Question } from './question-model';
 @Component({
   selector: 'app-question-detail',
-  templateUrl: './question-detail.component.html'
+  templateUrl: './question-detail.component.html',
+  styleUrls:['./question-detail.component.css']
 })
 export class QuestionDetailComponent{
 question: Question = new Question(
