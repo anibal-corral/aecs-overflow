4bda06de829dd17c74a68cb0ba25257102368463
Anibal Corral Serrano <anibal.corral@gmail.com>
2020-09-20 13:06:00 -0300

    Agrega el arreglo de Respuestas

--- aecs-overflow/src/app/question/question-model.ts
+++ aecs-overflow/src/app/question/question-model.ts
@@ -1,19 +1,23 @@
 /*Esta clase representa las preguntas*/
+import {Answer} from '../answer/answer.model';
 export class Question {
   title:string;
   description:string;
   createdAt?:Date;
   icon?:string;
+  answers:Answer[];
 
   constructor(
     title:string,
     description:string,
     createdAt?:Date,
-    icon?:string
+    icon?:string,
+    answers?:Answer[]
   ){
     this.title=title;
     this.description=description;
     this.createdAt=createdAt;
     this.icon=icon;
+    this.answers=answers;
   }
 }
